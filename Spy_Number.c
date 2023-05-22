3334656af3a756a75c902a1883e1ee3d71242205
Rohit Sai Anjan.D <119723402+Rohit-05002@users.noreply.github.com>
2023-05-22 21:49:53 +0530

    #include<stdio.h>
    int main()
    {
        int n;
        int sum = 0, product = 1, last_digit;
        scanf("%d", &n);
        int temp = n;
        while( n > 0 )
        {
           last_digit = n % 10;
           sum = sum + last_digit;
           product = product * last_digit;
           n = n / 10;
        }
    
        if(sum == product){
            printf("Spy Number");
        }
        else{
            printf("Not Spy Number");
        }
    
        return 0;
    
    }

---
+++ Spy_Number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    int sum = 0, product = 1, last_digit;
+    scanf("%d", &n);
+    int temp = n;
+    while( n > 0 )
+    {
+       last_digit = n % 10;
+       sum = sum + last_digit;
+       product = product * last_digit;
+       n = n / 10;
+    }
+
+    if(sum == product){
+        printf("Spy Number");
+    }
+    else{
+        printf("Not Spy Number");
+    }
+
+    return 0;
+
+}
